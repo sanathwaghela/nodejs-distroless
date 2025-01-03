914552c76a623e5d4308d86a52d01554ef01a23c
coder_sam <sanathwaghela1@gmail.com>
2025-01-03 16:11:27 +0530

    added dockerfile and k8s deployment file

---
+++ script.sh
@@ -0,0 +1,23 @@
+docker build -t nodejs-distroless:latest -f Dockerfile-distroless .
+docker build -t nodejs:latest -f Dockerfile-normal  .
+
+trivy image nodejs-distroless:latest
+trivy image nodejs:latest
+
+docker run -d --name nodejs-distroless-container -p 3000:3000 nodejs-distroless:latest
+docker run -d --name nodejs-container -p 3001:3000 nodejs:latest
+
+
+# to debug normal image
+docker exec -it nodejs-container sh
+
+# to debug distroless image
+docker exec -it nodejs-distroless-container sh  # this will not work as distroless image does not have shell
+
+# will use cdebug to debug distroless image on docker 
+cdebug exec -it --image=nixery.dev/shell/vim/ps/tshark/curl nodejs-distroless-container
+
+#  will use cdebug to debug distroless image on k8s
+ cdebug exec --namespace=default --image=nixery.dev/shell/vim/ps/tshark/curl -it pod/nodejs-distroless-596b695965-9mwqs
+
+ 
